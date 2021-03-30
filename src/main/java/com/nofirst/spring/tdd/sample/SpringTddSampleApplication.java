cce746d9948ec31099b3992db450897d030d504f
qianhuihuiji <1025926969@qq.com>
2021-03-30 17:22:30 +0800

    init

---
+++ src/main/java/com/nofirst/spring/tdd/sample/SpringTddSampleApplication.java
@@ -0,0 +1,13 @@
+package com.nofirst.spring.tdd.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTddSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTddSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nofirst/spring/tdd/sample/SpringTddSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nofirst.spring.tdd.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
